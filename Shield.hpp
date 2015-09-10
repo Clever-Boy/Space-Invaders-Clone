7264a0e1f1d18017f837a1fc368700132e00a2b5
MORTAL2000 <mortal_1971@yahoo.com>
2015-09-10 06:23:03 +0300

    spaceing

--- Shield.hpp
+++ Shield.hpp
@@ -24,7 +24,6 @@ public:
 public:
 	explicit				Shield(Type type, const TextureHolder& textures);
 
-
 	unsigned int			getCategory() const override;
 	sf::FloatRect			getBoundingRect() const override;
 
@@ -32,6 +31,7 @@ public:
 private:
 	void					drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const override;
 	void 					updateCurrent(sf::Time dt, CommandQueue& commands) override;
+
 
 private:
 	Type					mType;
